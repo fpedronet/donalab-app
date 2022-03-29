e1e7993b62c668fbebf18f51fecd2287db8405df
Glenn Lozano <glozano@sistemas.com>
2022-03-29 12:21:16 -0500

    inicia de guardar

--- src/app/page/donante/aspiranteligth/caspiranteligth/caspiranteligth.component.ts
+++ src/app/page/donante/aspiranteligth/caspiranteligth/caspiranteligth.component.ts
@@ -7,9 +7,12 @@ import { NotifierService } from 'src/app/page/component/notifier/notifier.servic
 import { SpinnerService } from 'src/app/page/component/spinner/spinner.service';
 import { Combobox } from 'src/app/_model/combobox';
 import { Distrito } from 'src/app/_model/distrito';
+import { Persona } from 'src/app/_model/persona';
+import { Predonante } from 'src/app/_model/predonante';
 import { ComboboxService } from 'src/app/_service/combobox.service';
 import { PredonanteService } from 'src/app/_service/predonante.service';
 import { UsuarioService } from 'src/app/_service/usuario.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-caspiranteligth',
@@ -69,8 +72,6 @@ export class CaspiranteligthComponent implements OnInit {
       'Sexo': new FormControl({ value: '', disabled: false}),
       'FecNacimiento': new FormControl({ value: null, disabled: false}),
       'CodPais': new FormControl({ value: '', disabled: false}),
-      'CodDepartamento': new FormControl({ value: '', disabled: false}),
-      'CodProvincia': new FormControl({ value: '', disabled: false}),
       'CodDistrito': new FormControl({ value: '', disabled: false}),
       'Celular': new FormControl({ value: '', disabled: false}),
       'Telefono': new FormControl({ value: '', disabled: false}),
@@ -127,6 +128,7 @@ export class CaspiranteligthComponent implements OnInit {
 
   buscarDistritos(name: string): Distrito[]{    
     var results: Distrito[] = [];
+    debugger;
     if(name.length >= 3){
       const filtro = name.toLowerCase();
       results = this.distritos.filter(e => e.dist?.descripcion?.toLowerCase().includes(filtro));
@@ -140,6 +142,51 @@ export class CaspiranteligthComponent implements OnInit {
     if(d !== undefined && d !== null)
       result = d.dist?.descripcion! + ', ' + d.prov?.descripcion! + ', ' + d.dpto?.descripcion!;
     return result;
+  }
+
+  guardar(){
+    let model = new Predonante();
+
+    model.codigo = this.form.value['Codigo'].value;
+
+    let p = new Persona();
+    p.idePersona = 0;
+    p.tipDocu = this.form.value['TipDocu'].value;
+    p.numDocu = this.form.value['NumDocu'].value;
+    p.apPaterno = this.form.value['ApPaterno'].value;
+    p.apMaterno = this.form.value['ApMaterno'].value;
+    var nombres: string = this.form.value['Nombres'].value;
+    var posEspacio = nombres.indexOf(' ');
+    if(posEspacio !== -1){
+      p.primerNombre = nombres.substring(0, posEspacio);
+      p.segundoNombre = nombres.substring(posEspacio, nombres.length-posEspacio-1);
+    }
+    else{
+      p.primerNombre = nombres;
+      p.segundoNombre = '';
+    }
+    p.sexo = this.form.value['Sexo'].value;
+    p.fecNacimiento = this.form.value['FecNacimiento'].value;
+    p.codPais = this.form.value['CodPais'].value;
+    p.codDistrito = this.form.value['CodDistrito'].value;
+    p.celular = this.form.value['Celular'].value;
+    p.telefono = this.form.value['Telefono'].value;
+    p.correo1 = this.form.value['Correo'].value;
+
+    debugger;
+
+    this.spinner.showLoading();
+    this.predonanteService.guardar(model).subscribe(data=>{
+
+      this.notifier.showNotification(data.typeResponse!,'Mensaje',data.message!);
+
+      if(data.typeResponse==environment.EXITO){
+        //this.router.navigate(['/page/grupo']);
+        this.spinner.hideLoading();
+      }else{
+        this.spinner.hideLoading();
+      }      
+    });
   }
 
 }
