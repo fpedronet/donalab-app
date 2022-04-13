16f029cad2994015f11cb99fb9b264e1f5a40e20
fpedronet <36646447+fpedronet@users.noreply.github.com>
2022-04-13 14:40:52 -0500

    merge

--- src/app/page/donante/donacion/cdonacion/cdonacion.component.ts
+++ src/app/page/donante/donacion/cdonacion/cdonacion.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import forms from 'src/assets/json/formulario.json';
+
+import { SpinnerService } from 'src/app/page/component/spinner/spinner.service';
+import { NotifierService } from 'src/app/page/component/notifier/notifier.service';
+import { UsuarioService } from 'src/app/_service/usuario.service';
+import { ConfigPermisoService } from 'src/app/_service/configpermiso.service';
+
+import { Combobox } from 'src/app/_model/combobox';
+import { environment } from 'src/environments/environment';
+import { Permiso } from 'src/app/_model/permiso';
 
 @Component({
   selector: 'app-cdonacion',
@@ -7,11 +19,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CdonacionComponent implements OnInit {
 
+  form: FormGroup = new FormGroup({});
+  permiso: Permiso = {};
+
+  nombres: string = "";
+  documento: string ="";
+
   constructor(
-    
+    private route: ActivatedRoute,
+    private router: Router,
+    private spinner: SpinnerService,
+    private notifierService : NotifierService,
+    private usuarioService: UsuarioService,
+    private configPermisoService : ConfigPermisoService,
   ) { }
 
   ngOnInit(): void {
+
+    this.inicializar();
+
+    this.obtenerpermiso();
+  }
+
+  inicializar(){
+    this.form = new FormGroup({
+      'idePreDonante': new FormControl({ value: '', disabled: false}),
+    });
+
+  }
+
+  obtener(){
+
+  }
+
+  obtenerpermiso(){
+    this.configPermisoService.obtenerpermiso(forms.entrevista.codigo).subscribe(data=>{
+      this.permiso = data;
+    });   
+  }
+
+  guardar(){
+
   }
 
 }
