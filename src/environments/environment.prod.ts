c2f9d4329883cb9279f08e0c4f50b2e317004f7b
fpedronet <36646447+fpedronet@users.noreply.github.com>
2022-08-05 17:01:36 -0500

    merge

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
 
+  UrlApi:'http://192.168.150.160:8022/api',  
   // UrlApi:'https://service.poclab.pe/donalab/api',  
-  // UrlImage:'assets/',
+  // UrlApi:'https://sitedev.poclab.pe/donalabdev/api', 
 
-  UrlApi:'https://sitedev.poclab.pe/donalabdev/api',  
   UrlImage:'assets/',
 
   //  UrlApi:'http://localhost:48394/api',
